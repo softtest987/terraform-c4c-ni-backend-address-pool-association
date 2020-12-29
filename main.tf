42be2987fe6e1dd216dc41bdc3b26cfe4c63f9b6
softtest987 <75788208+softtest987@users.noreply.github.com>
2020-12-29 18:45:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "azurerm_network_interface" "network_interface" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "link" {
-  network_interface_id    = var.backend_address_pool_id
+  network_interface_id    =  data.azurerm_network_interface.network_interface.id
   ip_configuration_name   = var.ip_configuration_name
   backend_address_pool_id = var.backend_address_pool_id
 }
